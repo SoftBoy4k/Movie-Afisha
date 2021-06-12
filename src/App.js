507e75d1d85b125c7c8346fbbb7a91e8443ce120
Vladislav <vladislavpimoshenko@gmail.com>
2021-06-12 19:39:07 +0300

    first routing update

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router,
-    Route,
-    Switch,
-    NavLink
-} from 'react-router-dom';
-import mainPage from './Pages/mainPage';
-import moviePage from './Pages/moviePage';
-import purchasePage from './Pages/purchasePage';
+import AppRouting from './components/AppRouting';
+
 
 export default class App extends Component {
     render(){
         
         return (
-            <Router>
-
-                <nav>
-                    <NavLink exact to="/">Home</NavLink>
-                    <NavLink to="/movie">Movie</NavLink>
-                    <NavLink to="/purchase">Purchase</NavLink>
-                </nav>
-
-                <Switch>
-                    <Route path="/" component={mainPage} exact />
-                    <Route path="/movie" component={moviePage} />
-                    <Route path="/purchase" component={purchasePage} />
-                </Switch>
-            </Router>
+            <AppRouting />
         );
     }
 }
